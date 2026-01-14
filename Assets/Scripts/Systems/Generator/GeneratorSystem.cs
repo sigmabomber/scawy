f30fcd1c89da82181cf59400852067d4406cd8bd
tommybomber <sinanabdullah@elev.ga.lbs.se>
2026-01-14 10:42:06 +0100

    Revert "eg"
    This reverts commit 905a14b892b24893f2d033f02fe6753c7f17eba2.

--- Assets/Scripts/Systems/Generator/GeneratorSystem.cs
+++ Assets/Scripts/Systems/Generator/GeneratorSystem.cs
@@ -3,28 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Doody.GameEvents.Generator;
-public class GeneratorSystem : EventListener, IInteractable
+public class GeneratorSystem : EventListener
 {
     private void Start()
     {
-       
+       // Events.Subscribe<Generator>
     }
-
-    public void Interact()
-    {
-
-    }
-    public bool CanInteract()
-    {
-        return true;
-    }
-    public string GetInteractionPrompt()
-    {
-        return null;
-    }
-    public Sprite GetInteractionIcon()
-    {
-        return null;
-    }
-
 }
