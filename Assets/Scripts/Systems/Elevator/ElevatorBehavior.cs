168a1e4406a19a32800c7ebc799bdcd39cf63869
Verl <vladziroblox@gmail.com>
2026-02-25 19:10:13 +0100

    pushcchc

--- Assets/Scripts/Systems/Elevator/ElevatorBehavior.cs
+++ Assets/Scripts/Systems/Elevator/ElevatorBehavior.cs
@@ -1,7 +1,6 @@
 using Doody.InventoryFramework;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
